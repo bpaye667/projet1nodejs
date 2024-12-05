dc04f9eadf12e922e411204f19b6ee7a1e927915
bpaye667 <paye-3950057@bemtech.sn>
2024-12-05 17:30:32 +0100

    16h30
    ajout

--- authen.js
+++ authen.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const { login, register } = require('../controllers/authController');
+const { login, register } = require('./controllerr');
 const router = express.Router();
 
-// Inscription d'un utilisateur
 router.post('/register', register);
-
-// Connexion
 router.post('/login', login);
 
 module.exports = router;
