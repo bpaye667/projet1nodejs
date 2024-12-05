ce913288a220eba9d6a4df297f71cdd0c3042f91
bpaye667 <paye-3950057@bemtech.sn>
2024-12-05 23:46:39 +0100

    22h47
    new add

--- authmiddleware.js
+++ authmiddleware.js
@@ -16,4 +16,4 @@ exports.isAuthenticated = (req, res, next) => {
 exports.isAdmin = (req, res, next) => {
     if (req.user.role !== 'admin') return res.status(403).json({ message: 'Accès interdit' });
     next();
-};
+}
