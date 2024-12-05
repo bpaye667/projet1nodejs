602f27339fad3cae15d1c6cb13f1f757f77d7e05
bpaye667 <paye-3950057@bemtech.sn>
2024-12-05 17:38:44 +0100

    16h38
    new add

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/users');
+const userRoutes = require('./user');
+const authRoutes = require('./authen');
 
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
+app.use('/auth', authRoutes); // Authentification
+app.use('/users', userRoutes); // Gestion des utilisateurs
 
+// Démarrer le serveur
 app.listen(3000, () => {
-    console.log('Serveur démarré sur le port 3000');
+    console.log('Serveur démarré sur http://localhost:3000');
 });
