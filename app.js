9b73ab607d50dd52c11568090eac0f5916914064
bpaye667 <paye-3950057@bemtech.sn>
2024-12-14 11:49:33 +0100

    10h48
    new add

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const userRoutes = require('./user');
-const authRoutes = require('./authen');
-
-const app = express();
-app.use(express.json());
-
-// Routes
-app.use('/auth', authRoutes); // Authentification
-app.use('/users', userRoutes); // Gestion des utilisateurs
-
-// Démarrer le serveur
-app.listen(3000, () => {
-    console.log('Serveur démarré sur http://localhost:3000');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const btnLogin = document.getElementById('btnLogin');
+    const btnRegister = document.getElementById('btnRegister');
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+  
+    btnLogin.addEventListener('click', () => {
+      loginForm.classList.remove('hidden');
+      registerForm.classList.add('hidden');
+    });
+  
+    btnRegister.addEventListener('click', () => {
+      registerForm.classList.remove('hidden');
+      loginForm.classList.add('hidden');
+    });
+  });
+  
